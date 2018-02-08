5ac00917b41b41db59868d8e74413aa7dad793a3
Zeal <mrkingtero@yahoo.com>
2018-02-08 13:46:07 -0800

    update case

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require("morgan");
+const uri = process.env.MONGODB_URI || 'mongodb://heroku_fjfgpqng:ucqd8rktijtr4a4arrkksubv8a@ds229878.mlab.com:29878/heroku_fjfgpqng'
 
-mongoose.connect('mongodb://heroku_fjfgpqng:ucqd8rktijtr4a4arrkksubv8a@ds229878.mlab.com:29878/heroku_fjfgpqng');
+mongoose.connect(uri);
 mongoose.Promise = Promise;
 
 const app = express();
